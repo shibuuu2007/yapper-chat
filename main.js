7fddad76c0f5b2d58ea14c0be3e42050e927c88e
shibuuu2007 <shivamishraq409@gmail.com>
2025-12-30 00:53:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  // Create the browser window
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  // Load the app from your local server
+  win.loadFile('loading.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
