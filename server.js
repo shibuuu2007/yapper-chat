cfc0a5e12d258815698e35edc268557f56905e63
shibuuu2007 <shivamishraq409@gmail.com>
2025-12-31 15:20:59 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const io = new Server(server, { cors: { origin: "*" } });
 // Access the Key from Render's Environment Variables
 const GEMINI_API_KEY = "AIzaSyCR_jfy2kPL7POGjp53tBCmCSsj-nEA2hI"; 
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-flash-latest" });
 
 // --- DATABASE ---
 const db = new Client({
